d64162b9144d8e75a145987ef1169a7084f9f582
levideutsch <levideutsch@live.com>
2022-09-04 23:20:49 -0400

    commit tree e870b4a5b791ed228d93d0d4889e2375cb0e6f6f

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(currentLocation){
+   if (currentLocation >= 42) {
+    return currentLocation - 42 //Test one = 1 block //Test two = 8 blocks
+} else {
+  return 42 - currentLocation //Test three = 8 blocks
+ }
+}
+
+   function distanceFromHqInFeet(currentLocation)  {
+    return distanceFromHqInBlocks(currentLocation)*264 //Test = 1 block * 264 feet
+} 
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+   return (destination - start)*264 //Test one = (one block)1320 feet //Test two = (5 blocks)2640 feet
+} else {
+   return (start - destination)*264 //Test three = (6 blocks)1584 feet
+}
+}
+
+function calculatesFarePrice(start, destination) {
+    let feet = distanceTravelledInFeet(start, destination)
+    if (feet < 400) {
+    return 0
+    
+} else if (feet > 400 && feet < 2000) {
+    return (feet - 400) * 0.02
+} else if (feet > 2000 && feet < 2500) {
+    return 25 
+} else if (feet > 2500) {
+    return "cannot travel that far"
+}
+
+}
+
+
+   
+
+
+
+
